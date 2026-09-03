94ad93b937acffb985f55c8da0827ab3d94c1555
Dywxty <leticia.talachia@gmail.com>
2026-09-03 08:43:16 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function App() {
+    return (
+        <View>
+            <Text>Olá, sou o aplicativo de CEP!!😊</Text>
+        </View>
+    );
+}
